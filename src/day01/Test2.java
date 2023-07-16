59570730c895cdc5e80d339895bcaa349e9bf05a
Özkan Sekmen <125397674+OzkanSekmen@users.noreply.github.com>
2023-07-16 13:48:11 +0300

    Update Test2.java

--- src/day01/Test2.java
+++ src/day01/Test2.java
@@ -1,12 +1,11 @@
 package day01;
 
-import utilities.StringUtility;
 
 public class Test2 {
     public static void main(String[] args ){
 
         String str = "Wooden Spoon";
-        StringUtility.printEachChar(str);
+        System.out.println(str);
 
     }
 }
