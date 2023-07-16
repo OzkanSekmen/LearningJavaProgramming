1b366d4b7f464eb1668e1b99d384434d824f469a
Ozkan Sekmen <sekmenozkan@hotmail.com>
2023-07-16 13:34:48 +0300

    Test2 class is created and completed

---
+++ src/day01/Test2.java
@@ -0,0 +1,12 @@
+package day01;
+
+import utilities.StringUtility;
+
+public class Test2 {
+    public static void main(String[] args ){
+
+        String str = "Wooden Spoon";
+        StringUtility.printEachChar(str);
+
+    }
+}
